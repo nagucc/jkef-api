992380d28c7756f46978f2ff6cb7044486ad6eef
na57 <na57@qq.com>
2017-06-13 12:28:23 +0800

    启用

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -38,7 +38,13 @@ router.get('/wxapp',
   async (req, res) => {
     res.json({
       ret: SUCCESS,
-      data: [],
+      data: [{
+        id: 'jkef-jxj',
+        title: '升学考试录取信息填写',
+        desc: '2017年度基金会奖学金发放工作即将开始，请您抓紧时间登记录取信息。',
+        faClass: 'fa fa-gift fa-3x',
+        url: '/pages/cee/registration/registration',
+      }],
     });
   }
 );
